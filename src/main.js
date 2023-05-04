1582ac2a6b8892cc1901d4559c44ef3d8e9346f7
Samuel Zhang <sashzhan@ucsc.edu>
2023-05-03 18:54:57 -0700

    added keys and UI

--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ let config = {
 }
 
 let game = new Phaser.Game(config);
+
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+//reserve keyboard variables
+let keyLEFT, keyRIGHT;
