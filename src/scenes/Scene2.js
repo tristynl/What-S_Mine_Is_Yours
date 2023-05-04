b50f70c3aa9d5e820eafe456757a897975f25ed4
tristynl <tristyn-lai@outlook.com>
2023-05-03 21:23:53 -0700

    Worked on Scene 2

---
+++ src/scenes/Scene2.js
@@ -0,0 +1,28 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super('Scene1');
+    }
+
+    preload() {
+        this.load.image('bedroom', './assets/bedroom.png');
+        
+    }
+
+    create() {
+        //add background to scene
+        this.add.image(0, 0, 'bedroom').setOrigin(0, 0);
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        //if condition, if player hits the side of screen
+        
+        //this.scene.start('Scene2');
+        /*if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.scene.start("Scene2");
+        }*/
+    }
+}
