e0e94cd13193ee2386a90a1c91ea6e79a593202e
Samuel Zhang <sashzhan@ucsc.edu>
2023-05-03 19:01:24 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -48,7 +48,7 @@ class Menu extends Phaser.Scene {
     }
 
     update() {
-        if(Phaser.InputKeyboard.isDown(keySPACEBAR)){
+        if(Phaser.InputKeyboard.JustDown(keySPACEBAR)){
             this.isCool = true;
             console.log(this.isCool);    
         }
