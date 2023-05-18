200ec86ab217fe188ae43097b981f347e7b1c98e
tristynl <tristyn-lai@outlook.com>
2023-05-18 11:11:16 -0700

    Tried to fix scene 2

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -9,16 +9,13 @@ class Scene2 extends Phaser.Scene {
         this.load.image('bedroom1', './assets/bedroom.png');
 
          //Character
-         this.load.image('girl', './assets/player1.png');
+         this.load.image('girl2', './assets/player1.png');
         
     }
 
     create() {
         //add background to scene
         this.add.image(0, 0, 'bedroom1').setOrigin(0, 0);
-        console.log("We at scene 2 y'all");
-        this.text2 = this.add.text(500, 50);
-        this.text2.setText(`Press left/right keys \n to move Player`);
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -26,18 +23,12 @@ class Scene2 extends Phaser.Scene {
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
         //For person
-        this.p1 = new Character(this, 680, 200, 'girl').setOrigin(0, 0);
-
-        //this.p1 = new Character(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'girl').setOrigin(0, 0);
-
+        this.p1 = new Character(this, 680, 200, 'girl2').setOrigin(0, 0);
 
         //For text
         this.text3 = this.add.text(250, 250);
         this.text3.setText(`Press W to Wake Up Sam`);
-        this.text = this.add.text(200, 435, ' ',{
-            align: 'center',
-            color: 'pink',
-        }); //325, 435
+        this.text = this.add.text(200, 435, ' ',); //325, 435
         this.text1 = this.add.text(500, 470);
         
     }
@@ -46,7 +37,7 @@ class Scene2 extends Phaser.Scene {
        
         this.p1.update();
 
-        if (Phaser.Input.Keyboard.JustDown(keyW)) {
+        /*if (Phaser.Input.Keyboard.JustDown(keyW)) {
             this.text3.setText(` `);
             this.text1.setText(`Press D for More Dialogue`);
             this.text.setText(`I can’t wait to show them this \n outfit I’ve been saving!`);  
@@ -75,7 +66,7 @@ class Scene2 extends Phaser.Scene {
 
         if (Phaser.Input.Keyboard.JustDown(keyF) && this.counter >= 5) {
             this.scene.start('Scene3'); 
-        }
+        }*/
 
     }
 }
