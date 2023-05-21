20af08a22126fbd8838f0da80188ef3839e8dd15
tristynl <tristyn-lai@outlook.com>
2023-05-20 21:48:25 -0700

    Worked on Scene 4

--- src/scenes/Scene4.js
+++ src/scenes/Scene4.js
@@ -14,19 +14,27 @@ class Scene4 extends Phaser.Scene {
     create() {
         //add background to scene
         this.add.image(0, 0, 'bathroom').setOrigin(0, 0);
-        this.text2 = this.add.text(500, 50);
-        this.text2.setText(`Press left/right keys \n to move Player`);
-        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
          //For text
         this.text = this.add.text(180, 435, ' '); 
-        this.text1 = this.add.text(500, 470);
-        this.text1.setText(`Press D for More Dialouge`);
         this.text.setText(`Time to get ready!`);
 
         this.p1 = new Character(this, 680, 200, 'girlr').setOrigin(0, 0);
+
+        //Button
+        let buttonConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '20px',
+            color: '#fcd9ff', //#843605
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }  
+        const startButton = new Button(525, 460, '...', this, () => this.counter += 1);
 
     }
 
@@ -34,20 +42,15 @@ class Scene4 extends Phaser.Scene {
 
         this.p1.update();
 
-        if (Phaser.Input.Keyboard.JustDown(keyD)) {
-            this.counter += 1;
+    
+        if(this.counter == 1){
+           this.text.setText(` `);
         }
 
-        if(this.counter == 1){
-           this.text.setText(`Press F`);
-        }
-       
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if(this.counter >= 2){
             this.scene.start('Scene5');
 
         }
-        /*if (Phaser.Input.Keyboard.JustDown(keyF) && this.counter >= 1) {
-            this.scene.start('Scene5');    
-        }*/
+       
     }
 }
