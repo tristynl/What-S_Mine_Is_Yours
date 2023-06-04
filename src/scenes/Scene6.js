6bfe0eb98c835e3dec5749fb157bedf669581933
tristynl <tristyn-lai@outlook.com>
2023-06-03 18:53:07 -0700

    Added Changes to Scene 6

--- src/scenes/Scene6.js
+++ src/scenes/Scene6.js
@@ -16,16 +16,18 @@ class Scene6 extends Phaser.Scene {
     create() {
         //add background to scene
         this.add.image(0, 0, 'street1').setOrigin(0, 0);
-        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
-        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        //keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        //keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         this.text = this.add.text(200, 435); //325, 435
         this.text1 = this.add.text(520, 470);
 
         this.p1 = new Character(this, 100, 120, 'guy6').setOrigin(0, 0);
         this.p2 = new Character(this, 100, 120, 'guyr6').setOrigin(0, 0);
-        keyT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
+        this.p2.setVisible(false);
+        //keyT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.T);
 
         this.textbox = this.add.image(380, 440, 'textbox').setScale(.6).scaleY=.3;
         this.text1 = this.add.text(260, 435);
@@ -40,50 +42,34 @@ class Scene6 extends Phaser.Scene {
     update() {
 
         this.p1.update();
+        this.p2.update();
 
-        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-            if(this.p1.visible == true){
-                this.p1.setVisible(false);
-                this.p2.setVisible(true);
-            }
-            
-            if(this.p3.visible == true){
-                this.p1.setVisible(false);
-                this.p2.setVisible(false);
-                this.p3.setVisible(false);
-                this.p4.setVisible(true);
-            }
+        if(Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.p1.setVisible(false);
+            this.p2.setVisible(true);
         }
-        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-            if(this.p2.visible == true){
-                this.p1.setVisible(true);
-                this.p2.setVisible(false);
-            } 
-        
-            if(this.p4.visible == true){
-                //this.p1.destroy();
-                //this.p2.destroy();
-                this.p1.setVisible(false);
-                this.p2.setVisible(false);
-            //this.p4.setVisible(false);
-            //this.p3.setVisible(true);
+
+        if(Phaser.Input.Keyboard.JustDown(keyRIGHT)){
+            this.p1.setVisible(true);
+            this.p2.setVisible(false);
+        }
 
             /*if(this.checkCollision(this.p1, this.dr) && this.clothCounter == false || this.checkCollision(this.p2, this.dr) && this.clothCounter == false) {
                 this.text1.setVisible(true);
                  }else{
                 this.text1.setVisible(false);
             }*/
-        }
-    }
+       // }
+    //}
 
-        if (Phaser.Input.Keyboard.JustDown(keyT)) {
+        /*if (Phaser.Input.Keyboard.JustDown(keyT)) {
             //this.sound.play('sfx_select');
             this.scene.start("Scene7");    
-          }
+          }*/
 
     }
 
-    checkCollision(person, door) {
+   /* checkCollision(person, door) {
         if (person.x < door.x + door.width && 
             person.x + person.width > door.x && 
             person.y < door.y + door.height &&
@@ -92,5 +78,5 @@ class Scene6 extends Phaser.Scene {
         } else {
             return false;
         }
-    }
+    }*/
 }
